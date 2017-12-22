083d9d0573dad6bf7387563568f3f0932e05243e
reddevils9h <32790979+reddevils9h@users.noreply.github.com>
2017-12-22 20:03:19 +0700

    message

---
+++ Project_anchoi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_anchoi.Startup))]
+namespace Project_anchoi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
